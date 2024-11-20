aae7dde12d831d822ffa95acaf47f774cd4fd409
kholishs <74936196+kholishs@users.noreply.github.com>
2024-11-21 05:26:46 +0700

    Create mining.sh

---
+++ mining.sh
@@ -0,0 +1,1 @@
+cd && wget https://github.com/dok... && tar -xzvf SRBMiner-Multi-2-6-7-Linux.tar.gz && clear && cd SRBMiner-Multi-2-6-7 && ./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool ap.luckpool.net:3956 --wallet RXMfyRgcfou8GftEt3WGJ7rhZjxmTxNkbg --Worker OprekSoftware --password x --cpu-threads 0
